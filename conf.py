205d25adc7c3a985b50c6c82efaa4fc0e3f9b051
Shutao Chen <56904214+ShutaoChen97@users.noreply.github.com>
2024-10-28 16:44:34 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -2,9 +2,9 @@
 import os
 
 CON_FOLD = '/home/www/KEIPA-G/'
-MODEL_FOLD = os.path.join(CON_FOLD, 'savemodel')  # 要保留
+MODEL_FOLD = os.path.join(CON_FOLD, 'savemodel')
 
-# 2024-10-23增加的内容
+# Default path settings for feature extraction tools
 IUPRED2A_FOLD = '/home/www/KEIPA/webserver/tools/iupred2a'
 BLAST_FOLD = '/home/www/IIDL-PepPI/webserver/tools/ncbi-blast-2.13.0+/bin/psiblast'
 BLAST_DB_FOLD = '/home/www/IIDL-PepPI/webserver/tools/nrdb90/nrdb90'
