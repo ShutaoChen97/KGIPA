1a94f13b717d56b590e9540a8162a7f3f6d91792
Shutao Chen <56904214+ShutaoChen97@users.noreply.github.com>
2024-10-28 16:45:08 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
 
-CON_FOLD = '/home/www/KEIPA-G/'
+CON_FOLD = '/home/www/KEIPA/'
 MODEL_FOLD = os.path.join(CON_FOLD, 'savemodel')
 
 # Default path settings for feature extraction tools
-IUPRED2A_FOLD = '/home/www/KEIPA/webserver/tools/iupred2a'
-BLAST_FOLD = '/home/www/IIDL-PepPI/webserver/tools/ncbi-blast-2.13.0+/bin/psiblast'
-BLAST_DB_FOLD = '/home/www/IIDL-PepPI/webserver/tools/nrdb90/nrdb90'
-BLOSUM62_FOLD = '/home/www/IIDL-PepPI/webserver/tools/blosum62.txt'
-SCRATCH_FOLD = '/home/www/IIDL-PepPI/webserver/tools/SCRATCH-1D_1.2'
-TRROSETTAX_FOLD = '/home/www/KEIPA/webserver/tools/trRosettaX'
-T5MODEL_FOLD = '/home/www/KEIPA/webserver/tools/prot_t5_xl_uniref50'
+IUPRED2A_FOLD = '/home/www/KEIPA/tools/iupred2a'
+BLAST_FOLD = '/home/www/KEIPA/tools/ncbi-blast-2.13.0+/bin/psiblast'
+BLAST_DB_FOLD = '/home/www/KEIPA/tools/nrdb90/nrdb90'
+BLOSUM62_FOLD = '/home/www/KEIPA/tools/blosum62.txt'
+SCRATCH_FOLD = '/home/www/KEIPA/tools/SCRATCH-1D_1.2'
+TRROSETTAX_FOLD = '/home/www/KEIPA/tools/trRosettaX'
+T5MODEL_FOLD = '/home/www/KEIPA/tools/prot_t5_xl_uniref50'
