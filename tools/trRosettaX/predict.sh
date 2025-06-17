c87787418f635911afe4a6c957741c614d51809c
Shutao Chen <shutao.chen@bit.edu.cn>
2025-06-17 10:20:44 +0800

    Update predict.sh

--- tools/trRosettaX/predict.sh
+++ tools/trRosettaX/predict.sh
@@ -2,4 +2,4 @@ echo $1 $2
 
 export PATH="/home/www/KGIPA/trxkeipa/bin/:$PATH"
 
-python /home/www/KGIPA/webserver/tools/trRosettaX/predict.py -i $1 -o $2 -mdir /mnt/home/webserver/model_res2net_202108 -cpu 30
+python predict.py -i $1 -o $2 -mdir model_res2net_202108 -cpu 30
