7e1ae5e469865a5719a9a07e534b257d02badd56
Shutao Chen <shutao.chen@bit.edu.cn>
2025-06-17 11:08:17 +0800

    Update SCRATCH.sh

--- tools/SCRATCH-1D_1.2/SCRATCH.sh
+++ tools/SCRATCH-1D_1.2/SCRATCH.sh
@@ -1,4 +1,3 @@
 echo $1 $2
 
-# ../bin/run_SCRATCH-1D_predictors.sh test.fasta test.out 4
-bash /home/www/IIDL-PepPI/webserver/tools/SCRATCH-1D_1.2/bin/run_SCRATCH-1D_predictors.sh $1 $2 1
+bash run_SCRATCH-1D_predictors.sh $1 $2 1
