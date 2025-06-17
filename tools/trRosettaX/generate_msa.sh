16d3d2cbb8c04f62689cfeb44d6c155902f2ecaa
Shutao Chen <shutao.chen@bit.edu.cn>
2025-06-17 10:32:57 +0800

    Update generate_msa.sh

--- tools/trRosettaX/generate_msa.sh
+++ tools/trRosettaX/generate_msa.sh
@@ -1,5 +1,5 @@
 echo $1 $2
 
-export PATH="/home/www/KGIPA/trxkeipa/bin/:$PATH"
+export PATH="/trxkeipa/bin/:$PATH"
 
 python generate_msa.py -i $1 -o $2 -hhdb /uniclust30_2018_08/uniclust30_2018_08 -e_value 0.001 -cpu 30
